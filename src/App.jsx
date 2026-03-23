7d170deee97952d7eff9a7d953be89addcec4f7f
rakinu21 <ralphkinethugdamin@gmail.com>
2026-03-23 12:09:57 +0800

    package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Loader from './components/Loader/Loader.jsx'
-import ThemeToggle from './components/ThemeToggle/ThemeToggle.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Projects from './pages/Projects.jsx'
+import GraphicDesign from './pages/GraphicDesign.jsx'
+import Contact from './pages/Contact.jsx'
+import AIChat from './components/AIChat/AIChat.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
 
 function App() {
   const [loaded, setLoaded] = useState(false);
